7622fe0d4ba25a15cf50e5023742afd5956570c7
niku_p <67380624+tongvist@users.noreply.github.com>
2021-11-12 19:10:44 +0200

    1.9 unicafe step 4 ja 1.10 unicafe step 5

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,16 +1,41 @@
 import React, {useState} from "react";
 
-const Statistics = ({name, value, unit}) => <p>{name}: {value}{unit}</p>
+const StatisticsLine = ({name, value, unit}) => <p>{name}: {value}{unit}</p>
+
+const Statistics = ({stats}) => {
+  const {good, neutral, bad, all, average, positive} = stats;
+  return all > 0 ? (
+    <div>
+      <StatisticsLine name="Good" value={good} />
+      <StatisticsLine name="Neutral" value={neutral} />
+      <StatisticsLine name="Bad" value={bad} />
+      <StatisticsLine name="All" value={all} />
+      <StatisticsLine name="Average" value={average} />
+      <StatisticsLine name="Positive" value={positive} unit="%"/>
+    </div>
+  ) : <p>No Feedback given</p>
+}
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  // Good = 1, neutral = 0, bad = -1
-  const all = good + neutral + bad; 
+  const all = good + neutral + bad;
   const average = (good - bad) / all;
-  const positive = (good / all) * 100;
+  const positive = (good / all) * 100
+
+  const stats = {
+    // Good = 1, neutral = 0, bad = -1
+      good: good,
+      neutral: neutral,
+      bad: bad,
+      all: all,
+      average: average,
+      positive: positive
+  }
 
   const handleGood = () => {
     setGood(good + 1);
@@ -26,18 +51,13 @@ const App = () => {
     <div>
       <h1>Give Feedback</h1>
 
-      <button onClick={handleGood}>Good</button>
-      <button onClick={handleNeutral}>Neutral</button>
-      <button onClick={handleBad}>Bad</button>
+      <Button onClick={handleGood} text="Good" />
+      <Button onClick={handleNeutral} text="Neutral" />
+      <Button onClick={handleBad} text="Bad" />
       
       <h1>Statistics</h1>
 
-      <Statistics name="Good" value={good} />
-      <Statistics name="Neutral" value={neutral} />
-      <Statistics name="Bad" value={bad} />
-      <Statistics name="All" value={all} />
-      <Statistics name="Average" value={average} />
-      <Statistics name="Positive" value={positive} unit="%"/>
+      <Statistics stats={stats} />
     </div>
   );
 }
