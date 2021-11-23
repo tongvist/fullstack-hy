6f2f8e45cf07f0274a557a04b8cbb5ae803f4d1a
niku_p <67380624+tongvist@users.noreply.github.com>
2021-11-23 21:17:20 +0200

    osa 4, tehtävä 4.4

--- osa4/app.js
+++ osa4/app.js
@@ -1,18 +1,18 @@
 const blogRouter = require('./controllers/blogs');
 const cors = require('cors');
 const config = require('./utils/config');
-const express = require("express");
+const express = require('express');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
 
 const app = express();
 
-logger.info('Connecting to MongoDB...')
+logger.info('Connecting to MongoDB...');
 mongoose.connect(config.MONGODB_URI)
-    .then(result => {
-        logger.info('Connected to MongoDB.')
-    })
-    .catch(error => logger.error('Error connecting to MongoDB', error));
+  .then(result => {
+    logger.info('Connected to MongoDB.');
+  })
+  .catch(error => logger.error('Error connecting to MongoDB', error));
 
 app.use(cors());
 app.use(express.json());
--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,7 +1,17 @@
 const dummy = (blogs) => {
-    return 1;
-}
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  let total = 0;
+
+  blogs.forEach(blog => {
+    total += blog.likes;
+  });
+
+  return total;
+};
 
 module.exports = {
-    dummy
-}
+  dummy, totalLikes
+};
