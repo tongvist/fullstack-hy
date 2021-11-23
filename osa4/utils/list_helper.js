76557862b3b428945dd93b51bcf1b63e01859201
niku_p <67380624+tongvist@users.noreply.github.com>
2021-11-23 21:46:33 +0200

    Osa 4, tehtävä 4.5

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -12,6 +12,23 @@ const totalLikes = (blogs) => {
   return total;
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return -1;
+  }
+
+  let maxLikes = 0;
+
+  blogs.forEach(blog => {
+    if (blog.likes > maxLikes) {
+      maxLikes = blog.likes;
+    }
+  });
+
+  return blogs.find(blog => blog.likes === maxLikes);
+
+};
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 };
