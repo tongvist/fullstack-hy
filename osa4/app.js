ffc88266f8bcb1841bdb6811d430ae6ade3635fc
niku_p <67380624+tongvist@users.noreply.github.com>
2021-11-26 20:33:59 +0200

    Osa 4, tehtävä 4.15

--- osa4/app.js
+++ osa4/app.js
@@ -1,4 +1,5 @@
 const blogRouter = require('./controllers/blogs');
+const userRouter = require('./controllers/users');
 const cors = require('cors');
 const config = require('./utils/config');
 const express = require('express');
@@ -17,5 +18,6 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors());
 app.use(express.json());
 app.use('/api/blogs', blogRouter);
+app.use('/api/users', userRouter);
 
 module.exports = app;
