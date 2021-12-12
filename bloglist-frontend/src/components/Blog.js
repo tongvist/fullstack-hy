61d86cd3a7755935613441e2a190f466e060afad
niku_p <67380624+tongvist@users.noreply.github.com>
2021-12-12 16:00:06 +0200

    Tehtävä 5.7

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,31 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [displayMode, setDisplayMode] = useState('short');
+  
+  if (displayMode === 'short') {
+    return (
+      <div className='blog'>
+        <p className='inline-p'>"{ blog.title }" by { blog.author }</p>
+        <button onClick={ () => setDisplayMode('full') }>View</button>
+      </div>  
+  )};
+
+  return (
+    <div className='blog'>
+      <p className='inline-p'>"{ blog.title }"</p>
+      <button onClick={ () => setDisplayMode('short') }>Hide</button>
+
+      <p>Author: { blog.author }</p>
+      
+      <p>URL: { blog.url }</p>
+      
+      <p className='inline-p'>Likes: { blog.likes }</p>
+      <button>like</button>
+      
+      <p>Added by: { blog.user.name }</p>
+    </div>
+  );
+};
+
+export default Blog;
