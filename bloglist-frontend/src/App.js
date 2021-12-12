a408235b78fa4157bbaafa483b8c0a8628b69e42
niku_p <67380624+tongvist@users.noreply.github.com>
2021-12-12 12:05:15 +0200

    Tehtävä 5.2

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Blog from './components/Blog';
 import blogService from './services/blogs';
 import loginService from './services/login';
+import './App.css';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
@@ -16,12 +17,23 @@ const App = () => {
     )  
   }, []);
 
+  useEffect(() => {
+    const loggedInUserJSON = window.localStorage.getItem('loggedInUser');
+    if (loggedInUserJSON) {
+      const user = JSON.parse(loggedInUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
+  }, []);
+
   const handleLogin = async (event) => {
     event.preventDefault();
     // console.log('Logging in with credentials: ', username, password);
 
     try {
       const user = await loginService.login({ username, password });
+      window.localStorage.setItem('loggedInUser', JSON.stringify(user));
+      blogService.setToken(user.token);
       setUser(user);
       setUsername('');
       setPassword('');
@@ -58,6 +70,11 @@ const App = () => {
     </form>
   );
 
+  const handleLogout = () => {
+    window.localStorage.clear();
+    window.location.reload();
+  }
+
   return (
     <div>
       <h2>blogs</h2>
@@ -65,7 +82,8 @@ const App = () => {
         loginForm() 
         :
         <div>
-          <p>{user.name} logged in</p>
+          <p className='logged-in-user'>{user.name} logged in</p>
+          <button onClick={handleLogout}>logout</button>
           {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
         </div>
       }
