0578ac80d98b875e5e699de22256928bf3db7534
niku_p <67380624+tongvist@users.noreply.github.com>
2021-12-27 16:32:44 +0200

    Tehtävä 6.5

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => state.sort((a, b) => {
+    return b.votes - a.votes;
+  }))
   const dispatch = useDispatch()
 
   const vote = (id) => {
