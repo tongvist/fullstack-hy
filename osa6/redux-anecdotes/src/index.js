72e72e436e3e2fe3e326e7552d8b5a7ee3caf05c
niku_p <67380624+tongvist@users.noreply.github.com>
2021-12-28 11:42:29 +0200

    Tehtävä 6.9

--- osa6/redux-anecdotes/src/index.js
+++ osa6/redux-anecdotes/src/index.js
@@ -1,15 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './App'
-import reducer from './reducers/anecdoteReducer'
+import App from './App';
+import { Provider } from 'react-redux';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import store from './store';
 
-const store = createStore(reducer)
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
-)
+);
