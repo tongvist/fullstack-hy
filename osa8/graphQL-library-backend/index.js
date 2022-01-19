7ee38553da776205b58149d8ae19af0f82bd283d
niku_p <67380624+tongvist@users.noreply.github.com>
2022-01-19 20:41:34 +0200

    Tehtävä 8.4

--- osa8/graphQL-library-backend/index.js
+++ osa8/graphQL-library-backend/index.js
@@ -87,7 +87,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
   type Book {
@@ -109,7 +109,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => args.author ? books.filter(b => b.author === args.author) : books,
     allAuthors: () => authors
   },
   Author: {
