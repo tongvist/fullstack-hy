6c38efdc3489f9261a8cbd4a723008946f37d0af
niku_p <67380624+tongvist@users.noreply.github.com>
2022-01-19 19:53:48 +0200

    Tehtävä 8.2

--- osa8/graphQL-library-backend/index.js
+++ osa8/graphQL-library-backend/index.js
@@ -87,13 +87,22 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
+  }
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
