f81c3f8e0b215dc6b60ae4f5239c967f0f6dda64
niku_p <67380624+tongvist@users.noreply.github.com>
2022-06-23 19:44:18 +0300

    Tehtävä 9.5

--- osa9/9_b/bmiCalculator.ts
+++ osa9/9_b/bmiCalculator.ts
@@ -1,7 +1,6 @@
-const calculateBmi = (height: number, weight: number) => {
+export const calculateBmi = (height: number, weight: number) => {
   // BMI: weight (kg) / height^2 (m)
   const bmi = weight / ((height / 100) ** 2);
-  console.log("BMI: ", bmi);
 
   return bmi > 25.0 ? "Overweight" : bmi < 18.5 ? "Underweight" : "Normal (healthy weight)";
 
@@ -9,7 +8,5 @@ const calculateBmi = (height: number, weight: number) => {
 
 const h = Number(process.argv[2]);
 const w = Number(process.argv[3]);
-
-console.log(h, w);
 
 console.log(calculateBmi(h, w));
--- osa9/9_b/index.ts
+++ osa9/9_b/index.ts
@@ -1,8 +1,32 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator';
+
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  const { height, weight } = req.query;
+
+  if (!height || !weight) {
+    return res.status(400).json({error: 'Missing height or weight information'});
+  };
+
+  if (isNaN(Number(height)) || isNaN(Number(weight))) {
+    return res.status(400).json({error: 'Height and weight should be numbers'});
+  };
+
+  const bmi = calculateBmi(Number(height), Number(weight));
+
+  const result = {
+    weight,
+    height,
+    bmi
+  }
+
+  return res.json(result);
 });
 
 const PORT = 3000;
